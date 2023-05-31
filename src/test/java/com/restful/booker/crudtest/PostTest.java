e173bd0856866c81cda8b3ea791aee70cee02221
mail2hardeek <mail2hardeek@gmail.com>
2023-05-31 18:33:22 +0100

    restful-booker-hw-wk17 Hardik Vaidya

---
+++ src/test/java/com/restful/booker/crudtest/PostTest.java
@@ -0,0 +1,45 @@
+package com.restful.booker.crudtest;
+
+import com.restful.booker.model.AuthorisationPojo;
+import com.restful.booker.model.BookingPojo;
+import io.restassured.response.Response;
+import org.junit.Test;
+
+import static io.restassured.RestAssured.given;
+
+public class PostTest {
+    @Test
+    public void createToken() {
+
+        AuthorisationPojo authorisationPojo = new AuthorisationPojo();
+        authorisationPojo.setUsername("admin");
+        authorisationPojo.setPassword("password123");
+        Response response = given()
+                .header("Content-Type", "application/json")
+                .when()
+                .body(authorisationPojo)
+                .post("https://restful-booker.herokuapp.com/auth");
+        response.then().statusCode(200);
+    }
+
+    @Test
+    public void createBooking() {
+
+        BookingPojo.BookingDates date = new BookingPojo.BookingDates();
+        date.setCheckin("2023-07-10");
+        date.setCheckout("2023-07-24");
+        BookingPojo bookingPojo = new BookingPojo();
+        bookingPojo.setFirstname("Harry");
+        bookingPojo.setLastname("VD");
+        bookingPojo.setTotalprice(11000);
+        bookingPojo.setDepositpaid(true);
+        bookingPojo.setBookingdates(date);
+        bookingPojo.setAdditionalneeds("Breakfast");
+        Response response = given()
+                .header("Content-Type", "application/json")
+                .when()
+                .body(bookingPojo)
+                .post("https://restful-booker.herokuapp.com/booking");
+        response.then().statusCode(200);
+    }
+}
